581e35b875279bf89d4bd850cf41967f7cb81f40
hathai191 <144460687+hathai191@users.noreply.github.com>
2023-10-26 16:19:25 +0200

    Updated weather.h
    Updated weather.h with Amanda and Ha part of the code.

--- include/weather.h
+++ include/weather.h
@@ -1,1 +1,52 @@
+#ifndef WEATHER_DATA_ANALYSIS_H
+#define WEATHER_DATA_ANALYSIS_H
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <stdexcept>
+
+struct TemperatureInfo {
+    double temperature;
+    std::string date;
+    std::string time;
+};
+
+class WeatherDataAnalysis {
+public:
+    WeatherDataAnalysis(const std::string& cleaneddata_Lund);
+
+    void readWeatherData();
+
+    double calculateAverageSpringTemperature(const WeatherDataAnalysis& weatherAnalysis) const;
+
+    const std::vector<int>& getYears() const {
+        return years;
+    }
+
+    const std::vector<int>& getMonths() const {
+        return months;
+    }
+
+    const std::vector<int>& getDays() const {
+        return days;
+    }
+
+    const std::vector<double>& getTemperatures() const {
+        return temperatures;
+    }
+
+private:
+    std::string filename;
+    std::vector<std::string> dates;
+    std::vector<std::string> times;
+    std::vector<double> temperatures;
+    std::vector<int> years;
+    std::vector<int> months;
+    std::vector<int> days;
+};
+
+#endif /* WEATHER_DATA_Analysis_H */
 
