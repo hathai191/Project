4d716299220b6443047f067600761ec590e74ce6
nathaly johansson <na3162jo-s@student.lu.se>
2023-10-27 03:29:23 +0200

    header, source, main and make files as well as macro

---
+++ Makefile
@@ -0,0 +1,30 @@
+CXX := g++
+CXXWARNINGS := -Wall -Wextra -Werror
+CXXOPT := -O3
+CXXSTD := -std=c++17
+INCLUDES := -I include
+
+CXXFLAGS := $(CXXWARNINGS) $(CXXSTD) $(CXXOPT) $(INCLUDES)
+LDFLAGS :=
+
+SRC_DIR := src
+BUILD_DIR := build
+
+SRCS := $(wildcard $(SRC_DIR)/*.cxx)
+OBJS := $(patsubst $(SRC_DIR)/%.cxx, $(BUILD_DIR)/%.o, $(SRCS))
+
+.PHONY: all clean
+
+all: main
+
+OBJS += $(BUILD_DIR)/cleanup_data.o
+
+main: main.cxx $(OBJS)
+	$(CXX) $(CXXFLAGS) -o $@ $^ $(LDFLAGS)
+
+$(BUILD_DIR)/%.o: $(SRC_DIR)/%.cxx
+	@mkdir -p $(dir $@)
+	$(CXX) $(CXXFLAGS) -o $@ -c $<
+
+clean:
+	rm -v $(BUILD_DIR)/*.o main
--- main.cxx
+++ main.cxx
@@ -1,50 +1,21 @@
-#include "weather.h"
 #include <iostream>
-#include <fstream>
-#include <sstream>
-#include <stdexcept>
-#include <vector>
-#include <string>
-#include <tuple>
+#include "temperature_data.h"
+#include "cleanup_data.h" // Include the cleanup function header
 
-int main(int argc, char* argv[]) {
-    if (argc != 2) {
-        std::cerr << "Usage: " << argv[0] << " <CSV filename>" << std::endl;
+int main() {
+    int result1 = processTemperatureData1();
+    int result2 = processTemperatureData2();
+    int result3 = processTemperatureData3();
+
+    if (result1 != 0 || result2 != 0 || result3 != 0) {
+        std::cerr << "Error processing data." << std::endl;
         return 1;
     }
 
-    const std::string filename = argv[1];
-    WeatherDataAnalysis weatherAnalysis(filename);
-    weatherAnalysis.readWeatherData();
+    std::cout << "Data processing completed successfully." << std::endl;
 
-    std::vector<std::tuple<std::string, int, double>> seasonAverages;
-
-    // Get the list of years from your data
-    std::vector<int> years = weatherAnalysis.getYears();
-
-    // Calculate and store average temperatures for each season and each year
-    const int startYear = 1780;
-    const int endYear = 2023;
-
-    for (int year = startYear; year <= endYear; year++) {
-        seasonAverages.push_back(std::make_tuple("Spring", year, weatherAnalysis.calculateAverageSeasonTemperature(3, 5, year)));
-        seasonAverages.push_back(std::make_tuple("Summer", year, weatherAnalysis.calculateAverageSeasonTemperature(6, 8, year)));
-        seasonAverages.push_back(std::make_tuple("Autumn", year, weatherAnalysis.calculateAverageSeasonTemperature(9, 11, year)));
-        seasonAverages.push_back(std::make_tuple("Winter", year, weatherAnalysis.calculateAverageSeasonTemperature(12, 2, year)));
-
-        // Output season averages for each year
-        for (const auto& entry : seasonAverages) {
-            std::string season;
-            int year;
-            double averageTemp;
-            std::tie(season, year, averageTemp) = entry;
-
-            std::cout << "Season: " << season << ", Year: " << year
-                    << ", Average Temperature: " << averageTemp << " °C" << std::endl;
-        }
-    }
-    // Create a histogram or perform further analysis as needed.
+    // Call the cleanup function from the cleanup_data.cxx
+    cleanupDataFiles();
 
     return 0;
 }
-
