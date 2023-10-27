5b5036ac9426ed75992cdbbc80d10f5669c708e1
AmAgVa <144016257+AmAgVa@users.noreply.github.com>
2023-10-27 22:19:57 +0200

    Deleted #include weather.h

--- main.cxx
+++ main.cxx
@@ -9,7 +9,6 @@
 #include <tuple>
 #include "temperature_data.h"
 #include "cleanup_data.h" // Include the cleanup function header
-#include "weather.h"
 
 int main(int argc, char* argv[]) {
     if (argc != 2) {
